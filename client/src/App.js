c02737b8a720984154b1c7cd494705065e7fb8ac
KyleAnthonyMcAdam <578403@student.belgiumcampus.ac.za>
2023-05-30 13:51:09 +0200

    validation of zip

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,39 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+function validateZipCode(zipCode) {
+  // Regular expression pattern to validate ZIP code
+  const zipCodePattern = /^\d{4}$/;
+  return zipCodePattern.test(zipCode);
+}
 
 function App() {
   const [backendData, setBackendData] = useState({});
   const [zipCode, setZipCode] = useState('');
+  const [invalidZipCode, setInvalidZipCode] = useState(false);
 
   useEffect(() => {
-    fetch(`/api?zip=${zipCode}`)
-      .then((response) => response.json())
-      .then((data) => {
-        setBackendData(data);
-      });
-  }, [zipCode]);
+    if (invalidZipCode) {
+      // Reset backendData if the ZIP code is invalid
+      setBackendData({});
+    } else {
+      fetch(`/api?zip=${zipCode}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setBackendData(data);
+        });
+    }
+  }, [zipCode, invalidZipCode]);
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
-    setZipCode(event.target.elements.zipCode.value);
+    const inputZipCode = event.target.elements.zipCode.value;
+    const isValidZipCode = validateZipCode(inputZipCode);
+    setInvalidZipCode(!isValidZipCode);
+    if (isValidZipCode) {
+      setZipCode(inputZipCode);
+    } else {
+      setBackendData({});
+    }
   };
 
   return (
@@ -23,7 +42,9 @@ function App() {
         <input type="text" name="zipCode" placeholder="Enter ZIP code" />
         <button type="submit">Submit</button>
       </form>
-      {typeof backendData.weather === 'undefined' ? (
+      {invalidZipCode ? (
+        <p>Please enter a valid ZIP code.</p>
+      ) : typeof backendData.weather === 'undefined' ? (
         <p>Loading...</p>
       ) : (
         <div>
